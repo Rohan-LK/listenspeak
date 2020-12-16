80e4be8cfddf98e7c608e7c49a64cf6cb07d598a
Rohan-LK <76035827+Rohan-LK@users.noreply.github.com>
2020-12-16 09:39:02 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="listenRspeak",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Rohan Lal Kshetry",
+    author_email="rlkshetry95@gmail.com",
+    description="speak and take command functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Rohan-LK/listenspeak.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
